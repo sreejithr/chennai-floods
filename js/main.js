d3983b3a3091b56f78960bdaf5f6d0e2528f640f
Sreejith <sreejith.r44@gmail.com>
2015-12-03 19:13:16 +0530

    Pointed the url to data/ for the geojson file

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ var Chennai = function() {
     this.render = function() {
         var self = this;
         var map = self.displayAndGetMap(document.getElementById("map"));
-        var geoJSONUrl = "/chennai-flooded-streets-Dec2.geojson";
+        var geoJSONUrl = "/data/chennai-flooded-streets-Dec2.geojson";
 
         $.ajax({
             url: geoJSONUrl,

9af8f1ffed2dc3cffcca297d59ca22b6ee37e475
Sreejith <sreejith.r44@gmail.com>
2015-12-03 19:39:16 +0530

    Back to root, geojson. This is getting really annoying

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ var Chennai = function() {
     this.render = function() {
         var self = this;
         var map = self.displayAndGetMap(document.getElementById("map"));
-        var geoJSONUrl = "/chennai-floods/data/chennai-dec2.geojson";
+        var geoJSONUrl = "/chennai-floods/chennai-dec2.geojson";
 
         $.ajax({
             url: geoJSONUrl,

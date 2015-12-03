ec0eee791a0f524f5481a281a22efa6a493db2fe
Sreejith <sreejith.r44@gmail.com>
2015-12-03 19:57:47 +0530

    Configured the search box

--- js/main.js
+++ js/main.js
@@ -21,6 +21,61 @@ var Chennai = function() {
         });
     }
 
+
+    this.configureSearchBox = function(map, el) {
+        var input = document.getElementById('place-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+        // Bias search within current bounding box
+        map.addListener('bounds_changed', function() {
+            searchBox.setBounds(map.getBounds());
+        });
+
+        var markers = [];
+        searchBox.addListener('places_changed', function() {
+            var places = searchBox.getPlaces();
+
+            if (places.length == 0) {
+              return;
+            }
+
+            // Clear out the old markers.
+            markers.forEach(function(marker) {
+              marker.setMap(null);
+            });
+            markers = [];
+
+            // For each place, get the icon, name and location.
+            var bounds = new google.maps.LatLngBounds();
+            places.forEach(function(place) {
+                var icon = {
+                    url: place.icon,
+                    size: new google.maps.Size(71, 71),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(17, 34),
+                    scaledSize: new google.maps.Size(25, 25)
+                };
+
+                // Create a marker for each place.
+                markers.push(new google.maps.Marker({
+                    map: map,
+                    icon: icon,
+                    title: place.name,
+                    position: place.geometry.location
+                }));
+
+                if (place.geometry.viewport) {
+                    // Only geocodes have viewport.
+                    bounds.union(place.geometry.viewport);
+                } else {
+                    bounds.extend(place.geometry.location);
+                }
+            });
+            map.fitBounds(bounds);
+        });
+    }
+
     this.render = function() {
         var self = this;
         var map = self.displayAndGetMap(document.getElementById("map"));
@@ -33,11 +88,7 @@ var Chennai = function() {
         });
 
         self.styleMap(map);
-
-        var input = document.getElementById('place-input');
-        var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
+        self.configureSearchBox(map, document.getElementById('place-input'));
     }
     
     this.render();
